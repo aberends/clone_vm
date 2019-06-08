2339d239e48821dd9983c509e07523aaf810364a
Allard Berends <aberends@casema.nl>
2019-06-08 12:29:01 +0200

    updates

---
+++ install_sat008.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DOMAIN=sat008
+POOL=centos
+
+virsh destroy $DOMAIN
+virsh undefine $DOMAIN
+virsh vol-delete $DOMAIN $POOL
+virsh vol-create-as $POOL $DOMAIN 200g
+
+MEMORY=$((10*1024))
+DESCRIPTION="Satellite 6.4 server"
+CDROM_PATH=/home/allard/clone_vm/rhel-server-7.6-x86_64-dvd.iso
+KICKSTART_PATH=/home/allard/clone_vm/$DOMAIN.ks
+MAC=$(printf "52:54:%2.2x:%2.2x:%2.2x:%2.2x\n" 192 168 122 8)
+
+virt-install \
+  --connect qemu:///system \
+  --name=$DOMAIN \
+  --memory=$MEMORY \
+  --arch=x86_64 \
+  --metadata=description="$DESCRIPTION" \
+  --vcpus=vcpus=1 \
+  --location=$CDROM_PATH \
+  --extra-args="ks=file:/$DOMAIN.ks console=ttyS0,115200 inst.sshd" \
+  --initrd-inject=$KICKSTART_PATH \
+  --os-variant=rhel7.6 \
+  --boot=hd \
+  --disk=vol=$POOL/$DOMAIN,device=disk,bus=virtio \
+  --network=network=default,mac=$MAC \
+  --graphics=none \
+  --noautoconsole \
+  --hvm \
+  --autostart
+
+while [ "$(virsh domstate $DOMAIN 2>/dev/null)" != "shut off" ]
+do
+  echo "Waiting for $DOMAIN to be shut off"
+  date
+  sleep 2
+done
+
+virsh autostart $DOMAIN
+virsh start $DOMAIN
